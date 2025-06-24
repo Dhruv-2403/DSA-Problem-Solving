fc712cd715c8f5be8b46a1d0fd182aae2a756174
Dhruv Sareen <dhruvsareen24@gmail.com>
2025-06-24 13:10:33 +0530

    Time: 96 ms (67.09%) | Memory: 21.3 MB (51.31%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -9,22 +9,17 @@ class Solution:
         for i in range(1,len(nums)):
             x=max(x+nums[i],nums[i])
             x1=max(x1,x)
-
-
-
-
-
             y=min(y+nums[i],nums[i])
             y1=min(y1,y)
         
             sum1+=nums[i]
 
-        # if x1<=0:
-        #     return x
+        if x1<=0:
+            return x1
 
         cir_sum=sum1-y1
-        if cir_sum==0:
-            return x1
+        # if cir_sum==0:
+        #     return x1
 
         return max(x1,cir_sum)
 
