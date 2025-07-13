00ebec09df3a5f76766da6008aa0eab5ea1b7b14
Dhruv Sareen <dhruvsareen24@gmail.com>
2025-07-13 18:06:42 +0530

    Time: 54 ms (58.52%) | Memory: 26.9 MB (84.33%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        count=1
-        r=1
 
-        nums.sort()
+        #O(n) due to hashmap function 
+        s=set(nums) #O(1)
+        count=0
+        for i in s:
+            if i-1 not in s:
+                next_i=i+1
+                l=1
+                while next_i in s: #every element doesnt needs to be checked , only the starting point should be checked
+                    l+=1
+                    next_i+=1
+                
+                count=max(count,l)
+        return count
 
-        if nums==[]:
-            return 0
-        for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]:
-                continue
-            if nums[i]==nums[i-1]+1:
-                count+=1
-            else:
-                count=1
-            r=max(r,count)
-        return r
+
         
