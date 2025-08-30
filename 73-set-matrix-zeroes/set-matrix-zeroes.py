9bbb8996b0d3057073d265e0eebda9d19fccfa66
Dhruv Sareen <dhruvsareen24@gmail.com>
2025-08-30 20:57:10 +0530

    Time: 2 ms (82.50%) | Memory: 13.1 MB (72.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,31 +1,40 @@
 class Solution(object):
     def setZeroes(self, matrix):
-
-        # zero_ro=[False]*len(matrix)
-        # zero_co=[False]*(len(matrix[0]))
-        x=[]
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """ 
+        n=len(matrix)
+        m=len(matrix[0])
+        flag_row=False
+        flag_col=False
+        for i in matrix[0]:
+            if i==0:
+                flag_row=True
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]==0:
-                    x.append((i,j))
-                    
-                else:
-                    continue
-        # return x
+            if matrix[i][0]==0:
+                flag_col=True
 
-        for i in range(len(x)):
-            a,b=x[i]
 
+     
+
+        for i in range(1,len(matrix[0])):
+            for j in range(1,len(matrix)):
+
+
+                if matrix[j][i]==0:
+                    matrix[0][i]=0  
+                    matrix[j][0]=0
+        for i in range(1,len(matrix[0])):
+            for j in range(1,len(matrix)):
+                if matrix[0][i]==0 or matrix[j][0]==0:
+                    matrix[j][i]=0
+        if flag_row:
+            for i in range(len(matrix[0])):
+                matrix[0][i]=0
+
+        
+        if flag_col:
             for i in range(len(matrix)):
-                for j in range(len(matrix[0])):
-                    if i==a or j==b:
-                        matrix[i][j]=0
-            
-        
-                
-        
-            
-
-
-        
-        
+                matrix[i][0]=0
+        return matrix
