fba077e77a56bfb132210d451b1d202eb27fd56a
Dhruv Sareen <dhruvsareen24@gmail.com>
2025-09-23 21:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.82%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+
+
+        version1=version1.split(".")
+        version2=version2.split(".")
+
+
+        # return version1
+        # return version1,version2
+        for i in range(max(len(version1),len(version2))):
+            
+            v1=int(version1[i])  if i<len(version1) else 0
+
+            v2=int(version2[i])  if i<len(version2) else 0
+            
+           
+            # print(len(version1))
+            
+            if v1<v2:
+                return -1
+            if v1>v2:
+                return 1
+        return 0
+          
+            
+        
+
+        
